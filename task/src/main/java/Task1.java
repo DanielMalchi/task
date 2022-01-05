0ca341f04abdd89fc2efb2510addfe34c0a8b85b
DoronKlein <96833330+DoronKlein@users.noreply.github.com>
2022-01-05 09:37:39 +0200

    Update Task1.java

--- task/src/main/java/Task1.java
+++ task/src/main/java/Task1.java
@@ -13,7 +13,7 @@ public class Task1 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Task1.main()");
+        System.out.println("Hello World");
     }
 
 }
